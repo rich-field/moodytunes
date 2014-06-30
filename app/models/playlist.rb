2ae37251daf2ff0f7deb353df241028a8299b359
TooDelirious <richfield14@gmail.com>
2014-07-01 09:38:12 +1000

    updated models

---
+++ app/models/playlist.rb
@@ -0,0 +1,15 @@
+# == Schema Information
+#
+# Table name: playlists
+#
+#  id         :integer          not null, primary key
+#  playlist_1 :string(255)
+#  playlist_2 :string(255)
+#  playlist_3 :string(255)
+#  created_at :datetime
+#  updated_at :datetime
+#
+
+class Playlist < ActiveRecord::Base
+  belongs_to :mood
+end
