8467c669f53cd86f851680bac772bb1b228ecdc6
TooDelirious <richfield14@gmail.com>
2014-07-01 19:18:57 +1000

    WORKING VICTORRRYYYYY

---
+++ app/controllers/moods_controller.rb
@@ -0,0 +1,18 @@
+class MoodsController < ApplicationController
+  def index
+    @moods = Mood.all
+  end
+
+  def show
+    @mood = Mood.where(:name => params[:id].capitalize).first
+    access_token = session[:at]
+    access_token_secret = session[:ats]
+      if access_token and access_token_secret
+        rdio = Rdio.new(["wckdv2pub6av9q5fgs22br49", "e8M57wnN4h"],
+                    [access_token, access_token_secret])
+
+        @currentUser = rdio.call('currentUser')['result']
+        @playlists = rdio.call('getPlaylists')['result']['owned']
+      end
+    end
+end
