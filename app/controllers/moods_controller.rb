3d2d0528490e5ab33434af027bca3e4a8ef204f6
tmitche <tmitche831@gmail.com>
2014-07-02 13:55:33 +1000

    style (WINNING)

--- app/controllers/moods_controller.rb
+++ app/controllers/moods_controller.rb
@@ -7,12 +7,16 @@ class MoodsController < ApplicationController
     @mood = Mood.where(:name => params[:id].capitalize).first
     access_token = session[:at]
     access_token_secret = session[:ats]
+    # raise params.inspect
       if access_token and access_token_secret
         rdio = Rdio.new(["wckdv2pub6av9q5fgs22br49", "e8M57wnN4h"],
                     [access_token, access_token_secret])
 
         @currentUser = rdio.call('currentUser')['result']
         @playlists = rdio.call('getPlaylists')['result']['owned']
+        # raise params.inspect
+      else
+        redirect_to user_login_path
       end
     end
 end
